a1a9a4c52c86b52514913d06785079631bd05fc6
Dirk Van Compernolle <compi@esat.kuleuven.ac.be>
2021-05-26 10:30:33 +0200

    x

--- .ipynb_checkpoints/setup-checkpoint.py
+++ .ipynb_checkpoints/setup-checkpoint.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 from spch_version import __version__   
 
 setup(
-    name="spchutils",
+    name="pyspch",
     version=__version__,
     url="",
 
@@ -12,7 +12,7 @@ setup(
     description="A loose collection of speech processing utilities",
     license = "free",
     
-    packages = ['spchutils'],
+    packages = ['pyspch'],
     # add spch_version to the required install modules
     py_modules = ['spch_version'],
     # a dictionary refering to required data not in .py files
@@ -21,8 +21,13 @@ setup(
     install_requires=[
         'numpy >= 1.15.0',
         'scipy >= 1.0.0',
-        'numba >= 0.43.0,<= 0.48.0',
+        'pandas >= 1.0',
+        'librosa >= 0.8.0',
+        'numba',
         'soundfile >= 0.9.0',
+        'matplotlib >= 3.1',
+        'ipywidgets >= 7.5.1',
+        'pydub'
     ],
     
     python_requires='>=3.6',
--- setup.py
+++ setup.py
@@ -22,12 +22,11 @@ setup(
         'numpy >= 1.15.0',
         'scipy >= 1.0.0',
         'pandas >= 1.0',
-        'librosa == 0.8.0',
-        'numba >= 0.43.0,<= 0.48.0',
+        'librosa >= 0.8.0',
+        'numba',
         'soundfile >= 0.9.0',
         'matplotlib >= 3.1',
         'ipywidgets >= 7.5.1',
-        'plotly >= 4.12',
         'pydub'
     ],
     
