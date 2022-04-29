993d045caa938b5488c830ad6eeebd0f5633f6b7
Dirk Van Compernolle <compi@esat.kuleuven.be>
2022-04-29 12:01:27 +0200

    Update __init__.py

--- pyspch/core/__init__.py
+++ pyspch/core/__init__.py
@@ -1,9 +1,10 @@
-__all__ =['constants','utils','audio','hillenbrand','sequence_data','file_tools','load_data','timit']
+__all__ =['constants','utils','audio','hillenbrand','sequence_data','file_tools','load_data','SpchData','timit']
 #
 from .file_tools import *
 from .constants import *
 from .utils import *
 from .load_data import *
+from .SpchData import *
 #
 from . import audio
 from . import timit
